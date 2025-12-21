5d0064b0bafc6d2d867b803506b10511292fbf66
Sjoerd Bolten <sjoerdabolten@gmail.com>
2025-12-21 06:41:11 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,16 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 export default defineConfig({
   plugins: [svelte()],
   server: {
+    allowedHosts: [
+      "localhost",
+      "127.0.0.1",
+      "0.0.0.0",
+      "10.0.1.158",
+      "mini.netlob",
+    ],
     proxy: {
       "/api": "http://localhost:8000",
-      "/eastereggs": "http://localhost:8000"
-    }
-  }
+      "/eastereggs": "http://localhost:8000",
+    },
+  },
 });
-
-
