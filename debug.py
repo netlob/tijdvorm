c78d91835419860980ab7af67e596f4523e81465
Sjoerd Bolten <sjoerdabolten@gmail.com>
2025-12-25 03:26:09 +0100

    .

--- debug.py
+++ debug.py
@@ -7,23 +7,26 @@ from time import sleep
 # from samsungtvws import SamsungTVWS
 
 
-sys.path.insert(0, '/Users/sjoerdbolten/Documents/fungits/samsung-tv-ws-api')
+# sys.path.insert(0, '/Users/sjoerdbolten/Documents/fungits/samsung-tv-ws-api')
 
 from samsungtvws import SamsungTVWS
 
 # Increase debug level
 logging.basicConfig(level=logging.INFO)
 
+print('connecting to tv')
+start_time = time.time()
 # Normal constructor
 tv = SamsungTVWS('10.0.1.111')
+print('connected to tv in', time.time() - start_time, 'seconds')
 
 # Is art mode supported?
-# info = tv.art().supported()
-# logging.info(info)
+info = tv.art().supported()
+logging.info(info)
 
 # List the art available on the device
-info = tv.art().available()
-logging.info(info)
+# info = tv.art().available()
+# logging.info(info)
 
 # # Retrieve information about the currently selected art
 # info = tv.art().get_current()
@@ -39,19 +42,24 @@ logging.info(info)
 # tv.art().select_image('SAM-S10002300', show=True)
 # sleep(5)
 # # Determine whether the TV is currently in art mode
-# info = tv.art().get_artmode()
-# logging.info(info)
+info = tv.art().get_artmode()
+logging.info(info)
 
 # Switch art mode on or off
 # tv.art().set_artmode(True)
 # tv.art().set_artmode(False)
 
 # # # Upload a picture
-# file = open('/Users/sjoerdbolten/Documents/Projects/tijdvorm/py/timeform_art.png', 'rb')
+# start_time = time.time()
+# print('uploading art')
+# file = open('rotated_freaky_bob.png', 'rb')
 # data = file.read()
 # content_id  = tv.art().upload(data, matte='none')
-# content_id = 'MY_F18998' # boef
-content_id = 'MY_F19054' # clock
+# print('uploaded art in', time.time() - start_time, 'seconds')
+
+# content_id = 'MY_F19168' # boef
+content_id = 'MY_F18998' # boef
+# content_id = 'MY_F19055' # clock
 start_time = time.time()
 print('setting art to', content_id)
 print(tv.art().select_image(content_id, show=True))
